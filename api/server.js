6e60847f0aa7c70c9b9e0caa829b6a16151d09d5
jeankhouri <hello@jeankhouri.com>
2024-10-14 18:08:12 -0400

    Code Cleanup

--- api/server.js
+++ api/server.js
@@ -1,27 +1,29 @@
-import express from 'express';
-import mongoose from 'mongoose';
-
-
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 async function run() {
-    const uri = process.env.DBURI
-    console.log(uri)
-    try {
-      // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-      await mongoose.connect(uri, clientOptions);
-      await mongoose.connection.db.admin().command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await mongoose.disconnect();
-    }
+  const clientOptions = {
+    serverApi: { version: "1", strict: true, deprecationErrors: true },
+  };
+  const uri = process.env.DBURI;
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 const app = express();
 
-
-app.listen(3000, () =>{
-    console.log("server listening on port 3000");
-})
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import Profile from './pages/Profile';
-import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
-export default function App(){
-return (
-<BrowserRouter>
-{/* header */}
-<Header></Header>
-  <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/about' element={<About />} />
-    <Route path='/signin' element={<Signin />} />
-    <Route path='/signup' element={<Signup />} />
-    <Route path='/profile' element={<Profile />} />
-  </Routes>
-</BrowserRouter>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      {/* header */}
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
